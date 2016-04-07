ecfad266c55e8c148ae9e26ed22dc2bcfd2b62ce
Jerry <Air@airde-macbook-air.local>
2016-04-07 20:18:46 +0800

    Initial Commit

---
+++ Xcode Demo app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Demo app
+//
+//  Created by Air on 2016/4/7.
+//  Copyright © 2016年 JF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
